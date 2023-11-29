ba62b4cd14a3436f7b3a191861cd8711128db6d9
gmalt02 <gaugemaltos13@gmail.com>
2023-11-28 21:21:24 -0600

    Update Library.java
    Added last implementation of Library class.

--- Library.java
+++ Library.java
@@ -1,13 +1,81 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Library {
-    // Add the missing implementation to this class
+    private String address;
+    private Map<String, Integer> catalog;
+    private List<String> openingHours;
+
+    public Library(String address) {
+        this.address = address;
+        this.catalog = new HashMap<>();
+        this.openingHours = new ArrayList<>();
+        initializeOpeningHours();
+    }
+
+    public Library(String address, String catalogFile) {
+        this(address);
+        loadCatalogFromFile(catalogFile);
+    }
+
+    public void addBook(Book book) {
+        catalog.put(book.getTitle(), catalog.getOrDefault(book.getTitle(), 0) + 1);
+    }
+
+    public void borrowBook(String title) {
+        if (catalog.containsKey(title) && catalog.get(title) > 0) {
+            System.out.println("You successfully borrowed " + title + ", remaining number of copies: " + (catalog.get(title) - 1));
+            catalog.put(title, catalog.get(title) - 1);
+        } else {
+            System.out.println("Sorry, this book is not in our catalog.");
+        }
+    }
+
+    public void returnBook(String title) {
+        catalog.put(title, catalog.getOrDefault(title, 0) + 1);
+        System.out.println("You successfully returned " + title + ", remaining number of copies: " + catalog.get(title));
+    }
+
+    public void printAvailableBooks() {
+        if (catalog.isEmpty()) {
+            System.out.println("No book in catalog");
+        } else {
+            for (Map.Entry<String, Integer> entry : catalog.entrySet()) {
+                System.out.println(entry.getKey() + ", remaining number of copies: " + entry.getValue());
+            }
+        }
+    }
+
+    public static void printOpeningHours() {
+        System.out.println("Libraries are open daily from 9am to 5pm.");
+    }
+
+    public void printAddress() {
+        System.out.println(address);
+    }
+
+    private void initializeOpeningHours() {
+        openingHours.add("Monday: 9am to 5pm");
+        openingHours.add("Tuesday: 9am to 5pm");
+        openingHours.add("Wednesday: 9am to 5pm");
+        openingHours.add("Thursday: 9am to 5pm");
+        openingHours.add("Friday: 9am to 5pm");
+        openingHours.add("Saturday: 9am to 1pm");
+        openingHours.add("Sunday: Closed");
+    }
+
+    private void loadCatalogFromFile(String catalogFile) {
+        // Implementation to load catalog from a file goes here
+        // For simplicity, this method is left empty in this example
+    }
 
     public static void main(String[] args) {
-        // Create two libraries
         Library firstLibrary = new Library("10 Main St.");
         Library secondLibrary = new Library("228 Liberty St.");
         Library thirdLibrary = new Library("12 Broadway St.", "catalog.txt");
 
-        // Add four books to the first library
         firstLibrary.addBook(new Book("The Da Vinci Code"));
         firstLibrary.addBook(new Book("The Da Vinci Code")); // second copy
         firstLibrary.addBook(new Book("Le Petit Prince"));
@@ -15,7 +83,6 @@ public class Library {
         firstLibrary.addBook(new Book("The Lord of the Rings"));
         firstLibrary.addBook(new Book("The Lord of the Rings")); // second copy
 
-        // Print opening hours and the addresses
         System.out.println("Library hours:");
         printOpeningHours();
         System.out.println();
@@ -26,7 +93,6 @@ public class Library {
         thirdLibrary.printAddress();
         System.out.println();
 
-        // Try to borrow The Lords of the Rings from both libraries
         System.out.println("Borrowing The Lord of the Rings:");
         firstLibrary.borrowBook("The Lord of the Rings");
         firstLibrary.borrowBook("The Lord of the Rings");
@@ -34,7 +100,6 @@ public class Library {
         secondLibrary.borrowBook("The Lord of the Rings");
         System.out.println();
 
-        // Print the titles of all available books from both libraries
         System.out.println("Books available in the first library:");
         firstLibrary.printAvailableBooks();
         System.out.println();
@@ -44,12 +109,10 @@ public class Library {
         thirdLibrary.printAvailableBooks();
         System.out.println();
 
-        // Return The Lords of the Rings to the first library
         System.out.println("Returning The Lord of the Rings:");
         firstLibrary.returnBook("The Lord of the Rings");
         System.out.println();
 
-        // Print the titles of available from the first library
         System.out.println("Books available in the first library:");
         firstLibrary.printAvailableBooks();
     }
